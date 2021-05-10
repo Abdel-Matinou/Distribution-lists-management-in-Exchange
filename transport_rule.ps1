7759c535cbe3500a3bdac3fdb359500f686c7354
abdel_matinou <akalahfabrice@gmail.com>
2021-05-10 00:57:57 +0000

    Editing Readme to add full description of the project and explain the subject

---
+++ transport_rule.ps1
@@ -0,0 +1,18 @@
+
+#Replace the following values
+$DLs_Emailaddresses = "Value1", "value 2"
+$AllowedDomains = "Value", "Value2", "Add your own domains + external domains"
+$Addressfor_IncidentReport = "Type an email address"
+$TransportRule_Name = "Type the transportRule name"
+
+# The following command will put any message to the DL in Quarantine or Deletethemessage without NDR, expect emails coming from allowed domains
+
+New-TransportRule 
+    -name $TransportRule_Name
+    -AnyOfToCcHeader $DLs_Emailaddresses 
+    -Quarantine <Boolean>   # True or False - choose if you want the message to be quarantined
+    -DeleteMessage <Boolean>  # True or False - choose if you want the message to be deleted without notifying the sender, I recommend this setting ON to prevent external senders to know if the email exists or not
+    -ExceptIfSenderDomainIs $AllowedDomains 
+    -Mode "Auditandnotify | Enforce" # Testmode or Apply the rule, First test and use Set-TransportRule to change the settings
+    -GenerateIncidentReport $Addressfor_IncidentReport
+    -IncidentReportContent ruledetections, sender, recipients, cc, attachOriginalMail
